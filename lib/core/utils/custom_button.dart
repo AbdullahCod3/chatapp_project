2d4356c30a45895d9fed1cd9b5394bfed2326af6
Abdullah <samybody11@hotmail.com>
2025-09-04 23:29:28 +0300

    welcome screen ui completed & constants file created & custom button created

---
+++ lib/core/utils/custom_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    super.key,
+    required this.title,
+    required this.color,
+    required this.onPressed,
+  });
+  final String title;
+  final Color color;
+  final Function onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Material(
+        elevation: 5,
+        color: color,
+        borderRadius: BorderRadius.circular(10),
+        child: MaterialButton(
+          onPressed: () => onPressed,
+          minWidth: 200,
+          height: 42,
+          child: Text(title, style: TextStyle(color: Colors.white)),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,3 +1,5 @@
+import 'package:chatapp_project/constants.dart';
+import 'package:chatapp_project/core/utils/custom_button.dart';
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatefulWidget {
@@ -11,7 +13,39 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [Column(children: [])]),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Column(
+              children: [
+                Container(height: 180, child: Image.asset('images/logo.png')),
+                Text(
+                  'MessageMe',
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.w900,
+                    color: Colors.purple[900],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 30),
+            CustomButton(
+              title: 'Sign in',
+              color: kPrimaryColor,
+              onPressed: () {},
+            ),
+            CustomButton(
+              title: 'Register',
+              color: kPrimaryColor,
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
